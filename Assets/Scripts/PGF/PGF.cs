81de5ee9e402a9a27979b8fc12e3adaad895a882
Carlos Michael Rodriguez <cmr624@nyu.edu>
2018-10-20 15:38:55 -0400

    added stuff and things

---
+++ Assets/Scripts/PGF/PGF.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PGF : MonoBehaviour
+{
+
+    public Projectile Fire(Vector3 direction, Vector3 position, int ammoRemaining)
+    {
+        var projectileObject = new GameObject()
+        {
+            name = "projectile", //tag = "projectile"
+        };
+        var projectileComponent = projectileObject.AddComponent<Projectile>();
+        //set projectile components
+        projectileComponent.DamageFunction = DamageFunction;
+        //we'll calculate the direction you're facing (first parameter) later, barrel's position
+        projectileComponent.ResolvePosition = ProjectileTrajectory(Vector3.zero, transform.position);
+
+        return projectileComponent;
+    }
+
+    // property of class, private variable public getter
+    // not serializable, you need to call the getter
+    public Func<float> DamageFunction { get; set; }
+
+    public Func<int, float> RateOfFireFunction { get; set; }
+
+                //parameters      //return value
+    public Func<Vector3, Vector3, Func<float, Vector3>> ProjectileTrajectory {get; set;}
+}
---
+++ Assets/Scripts/PGF/TestCases/TestPGFOne.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPGFOne : MonoBehaviour {
+    PGF PGFInstance;
+	// Use this for initialization
+    void Start () {
+        PGFInstance = PGFFactory.CreatePGF();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            PGFInstance.Fire(transform.forward, transform.position, 100);
+        }
+	}
+}
