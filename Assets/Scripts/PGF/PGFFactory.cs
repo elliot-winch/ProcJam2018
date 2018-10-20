620117ae20a9819810f0538dcd4635c1d9626f56
Carlos Michael Rodriguez <cmr624@nyu.edu>
2018-10-20 17:25:34 -0400

    framework day1

--- Assets/Scripts/PGF/PGFFactory.cs
+++ Assets/Scripts/PGF/PGFFactory.cs
@@ -3,44 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public static class PGFFactory {
+public class PGFFactory : MonoBehaviour{
 
-    public static PGF CreatePGF() {
-        var gameObject = new GameObject();
-        var pgf = gameObject.AddComponent<PGF>();
+    public static PGFFactory Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public GameObject pgfPrefab;
+
+    public PGF CreatePGF() {
+        var gameObjectInstance = Instantiate(pgfPrefab);
+        PGF pgf = gameObjectInstance.GetComponent<PGF>();
 
         //initialize by calling the generation
-        pgf.DamageFunction = CreateDamage();
-        pgf.RateOfFireFunction = CreateRateOfFire();
-        pgf.ProjectileTrajectory = CreateProjectileTrajectory();
+        pgf.DamageData = CreateDamage();
+        pgf.RateOfFireData = CreateRateOfFire();
+        pgf.ProjectileTrajectoryData = CreateProjectileTrajectoryData();
 
 
         return pgf;
     }
 
 
-    private static Func<Vector3, Vector3, Func<float, Vector3>> CreateProjectileTrajectory() {
+    private PGFProjectileTrajectoryData CreateProjectileTrajectoryData() {
         //jesus
-        return (direction, startPos) => { return (time) => {
-           //Debug.Log("CreateProjectileTrajectory is being called");
-                return Vector3.one * 0.6f; 
-            }; 
+        return new PGFProjectileTrajectoryData(){
+            speed = 1.0f, distanceBeforeSpread = 5.0f, spreadAngle = Mathf.PI / 4.0f
         };
     }
 
-    private static Func<float> CreateDamage(){
-        //lambda expression
-        Func<float> function = () => {
+    private PGFDamageData CreateDamage(){
 
-            return 1.0f;
+        return new PGFDamageData(){
+            baseDamage = UnityEngine.Random.Range(-1.0f, 1.0f),
+            damageDropOff = UnityEngine.Random.Range(-1.0f, 1.0f)
         };
-
-        return function;
     }
 
-    private static Func<int, float> CreateRateOfFire(){
-        Func<int, float> function = (ammoRemaining) => { return 1.0f; };
+    private PGFRateOfFireData CreateRateOfFire(){
 
-        return function;
+        return new PGFRateOfFireData()
+        {
+            baseRate = UnityEngine.Random.Range(0.0f, 1.0f),
+            ROFDataArr = new PGFBurstData[] {
+                new PGFBurstData (){
+                    n = 3,
+                    r = 2.0f,
+                }
+            }
+        };
     }
 }
